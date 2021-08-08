8f3443eb3aac39e6b9722fc7f46023e0ce54f837
nicovalencia <hello@nico.io>
2021-08-08 16:00:49 -0600

    Adds BYOFRecordsList schema. Adds BYOFRecord schema.

--- bin/bootstrap-ceramic.js
+++ bin/bootstrap-ceramic.js
@@ -4,6 +4,8 @@ import { createDefinition, publishSchema } from '@ceramicstudio/idx-tools'
 import { getIdx } from '../util/byof-marketplace-idx'
 import TrustedIdentitySchema from '../schemas/io.2c.hack-fs-2021.trustedIdentity'
 import TrustedIdentitiesListSchema from '../schemas/io.2c.hack-fs-2021.trustedIdentitiesList'
+import BYOFRecordSchema from '../schemas/io.2c.hack-fs-2021.BYOFRecord'
+import BYOFRecordsListSchema from '../schemas/io.2c.hack-fs-2021.BYOFRecordsList'
 
 async function bootstrap() {
   // Initialize an IDX instance with BYOFMarketplace seed:
@@ -15,15 +17,27 @@ async function bootstrap() {
   // Publish schemas:
   console.log( 'Publishing Schema: io.2c.hack-fs-2021.trustedIdentitiesList',)
   const [
-    trustedIdentitiesListSchema,
+    trustedIdentitiesListPublishedSchema,
   ] = await Promise.all([
     publishSchema(ceramic, { content: TrustedIdentitiesListSchema }),
   ])
   console.log( 'Publishing Schema: io.2c.hack-fs-2021.trustedIdentity',)
   const [
-    trustedIdentitySchema,
+    trustedIdentityPublishedSchema,
   ] = await Promise.all([
     publishSchema(ceramic, { content: TrustedIdentitySchema }),
+  ])
+  console.log( 'Publishing Schema: io.2c.hack-fs-2021.BYOFRecordsList',)
+  const [
+    BYOFRecordsListPublishedSchema,
+  ] = await Promise.all([
+    publishSchema(ceramic, { content: BYOFRecordsListSchema }),
+  ])
+  console.log( 'Publishing Schema: io.2c.hack-fs-2021.BYOFRecord',)
+  const [
+    BYOFRecordPublishedSchema,
+  ] = await Promise.all([
+    publishSchema(ceramic, { content: BYOFRecordSchema }),
   ])
 
   // Create definitions using the created schema ID:
@@ -31,13 +45,25 @@ async function bootstrap() {
   const trustedIdentitiesListDefinition = await createDefinition(ceramic, {
     name: 'trustedIdentitiesList',
     description: 'TrustedIdentitiesList',
-    schema: trustedIdentitiesListSchema.commitId.toUrl(),
+    schema: trustedIdentitiesListPublishedSchema.commitId.toUrl(),
   })
   console.log('Creating Definition: trustedIdentity')
   const trustedIdentityDefinition = await createDefinition(ceramic, {
     name: 'trustedIdentity',
     description: 'TrustedIdentity',
-    schema: trustedIdentitySchema.commitId.toUrl(),
+    schema: trustedIdentityPublishedSchema.commitId.toUrl(),
+  })
+  console.log('Creating Definition: BYOFRecordsList')
+  const BYOFRecordsListDefinition = await createDefinition(ceramic, {
+    name: 'BYOFRecordsList',
+    description: 'BYOFRecordsList',
+    schema: BYOFRecordsListPublishedSchema.commitId.toUrl(),
+  })
+  console.log('Creating Definition: BYOFRecord')
+  const BYOFRecordDefinition = await createDefinition(ceramic, {
+    name: 'BYOFRecord',
+    description: 'BYOFRecord',
+    schema: BYOFRecordPublishedSchema.commitId.toUrl(),
   })
 
   // Write config to JSON file:
@@ -46,10 +72,14 @@ async function bootstrap() {
     definitions: {
       trustedIdentity: trustedIdentityDefinition.id.toString(),
       trustedIdentitiesList: trustedIdentitiesListDefinition.id.toString(),
+      BYOFRecord: BYOFRecordDefinition.id.toString(),
+      BYOFRecordsList: BYOFRecordsListDefinition.id.toString(),
     },
     schemas: {
-      TrustedIdentity: trustedIdentitySchema.commitId.toUrl(),
-      TrustedIdentitiesList: trustedIdentitiesListSchema.commitId.toUrl(),
+      TrustedIdentity: trustedIdentityPublishedSchema.commitId.toUrl(),
+      TrustedIdentitiesList: trustedIdentitiesListPublishedSchema.commitId.toUrl(),
+      BYOFRecord: BYOFRecordPublishedSchema.commitId.toUrl(),
+      BYOFRecordsList: BYOFRecordsListPublishedSchema.commitId.toUrl(),
     },
   }
   await fs.writeFile(
