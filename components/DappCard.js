3dca32946c3ab78a44f4d17162b4e5ad601e28a5
nicovalencia <hello@nico.io>
2021-08-12 17:26:14 -0600

    Adds Web3.Storage frontend code loader/parser.

--- components/DappCard.js
+++ components/DappCard.js
@@ -1,10 +1,14 @@
+import FrontendCard from './FrontendCard'
+
 const DappCard = ({ contractAddress, ipfsAddresses }) => {
   return (
     <div>
       <h1>{contractAddress}</h1>
       <h2>IPFS Addresses</h2>
       <ul>
-        {Object.keys(ipfsAddresses).map(ipfsHash => <li key={ipfsHash}>{ipfsHash} - {ipfsAddresses[ipfsHash]} people</li>)}
+        {Object.keys(ipfsAddresses).map(ipfsAddress => 
+          <FrontendCard key={ipfsAddress} ipfsAddress={ipfsAddress} numberOfPeople={ipfsAddresses[ipfsAddress]} />
+        )}
       </ul>
     </div>
   )
